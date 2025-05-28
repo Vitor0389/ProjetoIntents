4ccc2a47e603f745c8bc8893ccbbc01c3c9b76e1
Vitor0389 <vitor0389@gmail.com>
2025-05-28 19:15:54 -0300

    começando projeto

---
+++ app/src/main/java/com/projetointents/MainActivity.kt
@@ -0,0 +1,9 @@
+package com.projetointents
+
+
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+
+}
---
+++ app/src/main/java/com/projetointents/ParameterActivity.kt
@@ -0,0 +1,13 @@
+package com.projetointents
+
+import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ParameterActivity : AppCompatActivity(){
+
+
+}
